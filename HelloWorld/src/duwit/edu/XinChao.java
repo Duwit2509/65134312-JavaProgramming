ada0228e97c63c04617fa9f04c3a5c542f6296fe
Duwit2509 <viettran25092005@gmail.com>
2026-01-26 18:26:16 +0700

    vi du dau tien
    chi de demo

---
+++ HelloWorld/src/duwit/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package duwit.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// 
+		System.out.println("Toi lan dau hoc JAVA, va su dung thu nghiem quan ly ma nguon voi GIT");
+	}
+
+}
